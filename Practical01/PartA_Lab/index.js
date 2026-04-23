0b891a6d79e8f39942bfdb1306b0d270b39e674e
Jay <jaylcx3@gmail.com>
2026-04-23 11:17:01 +0800

    completed practical01 partA_lab

---
+++ Practical01/PartA_Lab/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello from IT!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
